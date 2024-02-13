440af23df9429b54378c630f82e48f4f3779bcc2
rakshit agarwal <rakshitag13@gmail.com>
2024-02-13 23:05:00 +0530

    mern-estate

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "real-estate-59a97.firebaseapp.com",
+  projectId: "real-estate-59a97",
+  storageBucket: "real-estate-59a97.appspot.com",
+  messagingSenderId: "816312408985",
+  appId: "1:816312408985:web:2160fcb973c222aa2d5108"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
