2db54011022b061ccf8175bf2c957294401af8ee
Rakshit Agarwal <rakshitag13@gmail.com>
2024-02-15 21:38:08 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyChhNDCjaYJNyzzfqywPli8jhsiB7H03Kc",
   authDomain: "real-estate-59a97.firebaseapp.com",
   projectId: "real-estate-59a97",
   storageBucket: "real-estate-59a97.appspot.com",
